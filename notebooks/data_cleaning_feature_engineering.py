2611a2009ad672e921498d710ae5c540ab141fb5
Rutuja Deshmukh <rutujadeshmukh1516@gmail.com>
2026-05-23 17:42:50 +0530

    Update data_cleaning_feature_engineering.py

--- notebooks/data_cleaning_feature_engineering.py
+++ notebooks/data_cleaning_feature_engineering.py
@@ -1,1 +1,13 @@
+import pandas as pd
+import numpy as np
+df = pd.read_csv("hotel_bookings.csv")
+df.head()
+df.shape
+df.info()
+df.isnull().sum()
+df["children"] = df["children"].fillna(0)
+df["country"] = df["country"].fillna("Unknown")
+df["agent"] = df["agent"].fillna(0)
+df["company"] = df["company"].fillna(0)
 
+df = df.drop_duplicates()
