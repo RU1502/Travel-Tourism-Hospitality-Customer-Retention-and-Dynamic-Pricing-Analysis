5a9dbb6867a7223790799e86cad54e250a128c29
Rutuja Deshmukh <rutujadeshmukh1516@gmail.com>
2026-06-07 22:56:14 +0530

    Add SQL queries for booking analysis

--- sql/booking_analysis_queries.sql
+++ sql/booking_analysis_queries.sql
@@ -2,10 +2,12 @@
 SELECT * 
 FROM hotel_bookings 
 limit 10;
+
 ##2Count total bookings
 SELECT 
 COUNT(*) AS total_bookings
 FROM hotel_bookings;
+
 ##3 Count cancelled and non-cancelled bookings
 SELECT 
 is_canceled,
